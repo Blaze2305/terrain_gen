155175a0dea160fb09f768c367b4dd99708c14e3
Blaze2305 <prajpran@gmail.com>
2019-06-06 23:18:45 +0530

    static terrain gen

--- code/grid_points.js
+++ code/grid_points.js
@@ -2,7 +2,7 @@ class points{
   constructor(x,y,height){
       this.x=x;
       this.y=y;
-      this.height=height+ground_height;
+      this.height=height+ground_height/2;
   }
 
   show(){
@@ -10,6 +10,61 @@ class points{
     stroke(255);
     strokeWeight(4);
     point(this.x,this.height,this.y);
+    pop();
+  }
+
+  join(a,c,d){
+    push();
+    let g=0;
+    let b=0;
+    let avg1,avg2;
+    noStroke()
+
+    // for points this,a,c
+
+    if(this.height>0 && a.height>0 && c.height>0){
+      avg1=(this.height+a.height+c.height)/3;
+      g=map(avg1,0,mountain_height,110,255);
+    }
+    else{
+      b=map(this.height,sea_depth,0,0,255);
+    }
+
+    fill(0,g,b)
+    beginShape();
+    vertex(this.x,this.height,this.y);
+    vertex(a.x,a.height,a.y);
+    vertex(c.x,c.height,c.y);
+    endShape();
+
+    // for points this,c,d
+
+    if(this.height>0 && c.height>0 && d.height>0){
+      avg2=(this.height+d.height+c.height)/3;
+      g=map(avg2,0,mountain_height,110,255);
+    }
+    else{
+      b=map(this.height,sea_depth,0,0,255);
+    }
+      fill(0,g,b)
+    beginShape();
+    vertex(this.x,this.height,this.y);
+    vertex(d.x,d.height,d.y);
+    vertex(c.x,c.height,c.y);
+    endShape();
+    pop();
+  }
+
+  level(base){
+    this.height=base;
+  }
+
+  down(){
+    push();
+    stroke(255);
+    if(this.height>ground_height){
+      line(this.x,this.height,this.y,this.x,ground_height/2,this.y);
+    }
     pop();
   }
 }
--- code/sketch.js
+++ code/sketch.js
@@ -8,28 +8,38 @@ let square_width=60;
 let n=size/square_width;
 let ground_height=40;
 let i,j,k;
+let mountain_height=300;
+let sea_depth=-200
+let xoff;
+let yoff;
+
 
 function setup(){
 
   createCanvas(800,800,WEBGL);
+  let xoff=random(0,10);
+  let yoff=random(10,100000);
   x=createSlider(0,360,0,1);
   y=createSlider(0,360,0,1);
   z=createSlider(0,360,0,1);
   button=createButton("Switch between rotate and no rotate mode");
   button.mousePressed(change)
 
-  ground=new floors(0,0,0,ground_height,size);
+ ground=new floors(0,0,0,ground_height,size);
   for(i=0;i<n;i++){
     grid_points[i]=[]
     for(j=0;j<n;j++){
-      grid_points[i][j]=new points((floor(n/2)-i-1/2)*square_width,(floor(n/2)-j-1/2)*square_width,0);
+      let b=map(noise(xoff,yoff),0,1,sea_depth,mountain_height);
+      grid_points[i][j]=new points((floor(n/2)-i-1/2)*square_width,(floor(n/2)-j-1/2)*square_width,b);
+      xoff+=0.06
     }
+    yoff+=0.1
   }
 }
 
 function draw(){
   background(0);
-  if(num==1){
+  if(num==0){
     rotateX(radians(158+x.value()));
     rotateY(radians(y.value()));
     rotateZ(radians(z.value()));
@@ -40,15 +50,27 @@ function draw(){
   }
 
 
-  ground.show()
-  axes();
+ ground.show()
+  // axes();
   for(i=0;i<n;i++){
     for(j=0;j<n;j++){
-      grid_points[i][j].show()
+      grid_points[i][j].show();
     }
   }
 
-
+  for(i=0;i<n-1;i++){
+    for(j=0;j<n-1;j++){
+      grid_points[i][j].join(grid_points[i][j+1],grid_points[i+1][j+1],grid_points[i+1][j]);
+    }
+  }
+  for(i=0;i<n;i++){
+    grid_points[0][i].down();
+    grid_points[n-1][i].down();
+  }
+  for(i=0;i<n;i++){
+    grid_points[i][0].down();
+    grid_points[i][n-1].down();
+  }
 }
 
 
@@ -67,11 +89,12 @@ function change(){
 
 function axes(){
   push();
+  strokeWeight(5)
   stroke(255,0,0);
-  line(0,0,0,100,0,0);
+  line(0,0,0,300,0,0);
   stroke(0,255,0);
-  line(0,0,0,0,100,0);
+  line(0,0,0,0,300,0);
   stroke(0,0,255);
-  line(0,0,0,0,0,100);
+  line(0,0,0,0,0,300);
   pop();
 }
